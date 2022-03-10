db118396717dad4899baf1adc9c8fe70d6dd8574
dennis kimani <machariad196@gmail.com>
2022-03-10 11:33:29 +0300

    navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { LandingComponent } from './landing/landing.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:LandingComponent},
+  {path:'home',component:LandingComponent},
+  {path: 'navbar', component:NavbarComponent},
+  {path: 'skills',component:SkillsComponent},
+  {path:'projects',component:ProjectsComponent},
+  {path:'contact',component:ContactComponent},
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+  
+}
