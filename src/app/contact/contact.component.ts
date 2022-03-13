a01d27c8cd38b47e9863a2b6e4217469bbb454e6
dennis kimani <machariad196@gmail.com>
2022-03-13 21:29:11 +0300

    finishing on projects

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,21 @@
+
+  
 import { Component, OnInit } from '@angular/core';
+// import { ContactService } from '../service/contact.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {FormBuilder,FormGroup,} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+// import { TesterComponent } from '../tester/tester.component';
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +23,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
 
-  constructor() { }
+  matcher = new MyErrorStateMatcher();
+ form:any = {
+   name:null,
+   email:null,
+   subject:null,
+   message:null
+ }
+  constructor(/*private contactService:ContactService,*/private router:Router,private toastr: ToastrService, public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
-
+  onSubmit(): void{
+    let {name,email,subject,message}= this.form;
+    // this.contactService.post(name,email,subject,message).subscribe(
+    //   (data) => {
+    //     console.log(data)
+    //     this.toastr.success('Mwangaza Little Readers Received Your Message');
+    //   this.router.navigate (['contact'])
+       
+    //   },
+    //   (err) => {
+    //    console.log(err)
+    //    this.toastr.error('Check Your Details ');
+    //   });
+      // );
+      // this.dialogRef.close();
+  }
 }
--- src/app/projects/kinetik/kinetik.component.ts
+++ src/app/projects/kinetik/kinetik.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+
 import {MatBottomSheet, MatBottomSheetRef} from '@angular/material/bottom-sheet';
-import { ProjectsComponent } from '../projects.component';
+import { DialogData, ProjectsComponent } from '../projects.component';
+import { Component, Inject, OnInit } from '@angular/core';
+import { ScrollStrategy, ScrollStrategyOptions } from '@angular/cdk/overlay';
+
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
 
 @Component({
@@ -9,12 +13,18 @@ import { ProjectsComponent } from '../projects.component';
   styleUrls: ['./kinetik.component.css']
 })
 export class KinetikComponent implements OnInit {
-  [x: string]: any;
- 
 
-  constructor() { }
- 
+  constructor(
+    public dialogRef: MatDialogRef<ProjectsComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+    ) {}
+
+
   ngOnInit(): void {
+    
   }
-
+  
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
