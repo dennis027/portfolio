5c590e5a76af17c15e93b53f38355c50df96964d
dennis kimani <machariad196@gmail.com>
2022-03-14 01:32:41 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio';
+  navbarOpen = false;
+  toggleNavbar() {
+    this.navbarOpen =  !this.navbarOpen;
+  }
 }
