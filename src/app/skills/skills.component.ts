110456faea55ac95e166cb812258992b2325501d
dennis kimani <machariad196@gmail.com>
2022-03-13 11:28:58 +0300

    updating skills[C

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -6,8 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills.component.css']
 })
 export class SkillsComponent implements OnInit {
-
-  constructor() { }
+  showButton: boolean = false;
+  showDiv: boolean = false;
+  showDiv1: boolean = false;
+  showDiv2: boolean = false;
+  showDiv3: boolean = false;
+  showDiv4: boolean = false;
+  showDiv5: boolean = false;
+  showDiv6: boolean = false;
+  showDiv7: boolean = false;
+  showDiv8: boolean = false;
+  showDiv9: boolean = false;
+  showDiv10: boolean = false;
+  showDiv11: boolean = false;
+  changeText: boolean;
+  constructor() {
+     this.changeText = false;
+  }
 
   ngOnInit(): void {
   }
