c325b957b669ab7cf28eea829af89bd94c313cd1
dennis kimani <machariad196@gmail.com>
2022-03-30 17:10:16 +0300

    update

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Router } from '@angular/router';
+
+const AUTH_API = "https://dennis027portfolio.herokuapp.com/";
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private http: HttpClient, private router: Router,) { }
+
+  register(username: string, email: string, phone: string, role: string, password: string): Observable<any> {
+    return this.http.post(AUTH_API + 'register/', {
+      username,
+      email,
+      phone,
+      role,
+      password
+    }, httpOptions);
+  }
+
+
+  login(userdata: any): Observable<any> {
+    return this.http.post(AUTH_API + 'token/', userdata)
+  }
+
+  signOut(): void {
+    localStorage.clear();
+    this.router.navigate(['/']);
+
+  }
+
+  getUsers(): Observable<any[]> {           
+    return this.http.get<any[]>(AUTH_API + 'user/',);
+  }        
+                                                    
+}   
+
+
---
+++ src/app/service/contact.service.ts
@@ -0,0 +1,31 @@
+
+import { Injectable } from '@angular/core';
+import { HttpHeaders,HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+
+const CONTACT = "https://dennis027portfolio.herokuapp.com/";
+
+const httpOptions = {
+  headers: new HttpHeaders ({'Content-Type':'application/json'})
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+  private api = "https://dennis027portfolio.herokuapp.com/contact/";
+  constructor(private httpClient:HttpClient,private http:HttpClient, private router:Router) { }
+
+  getData(): Observable<any[]> {
+    return this.httpClient.get<any[]>(this.api);
+  }
+
+  post(name:string,email:string,subject:string,message:string): Observable<any>{
+    return this.http.post(CONTACT + 'contact/',{
+      
+      name,email,subject,message
+    },httpOptions)
+  }
+
+}
