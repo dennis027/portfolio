78448f5f33f7d8b6ba3b2cb3129fa94b3e81f11f
dennis <machariad196@gmail.com>
2026-06-30 01:27:05 +0300

    adding a route for portfolio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./portfolio/portfolio').then(
+        (m) => m.Portfolio
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
